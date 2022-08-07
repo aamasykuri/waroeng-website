e97fbdd76306088ed251a5ab5dc42d9d7c05316e
marginkuri <aagung53@gmail.com>
2022-08-07 14:30:10 +0700

    add pagination swipper.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
