2607c1fac90626c8ace50381996e0adf15af1345
marginkuri <aagung53@gmail.com>
2022-08-11 11:45:21 +0700

    fixed hero slider, add swiper.js

--- js/script.js
+++ js/script.js
@@ -19,19 +19,4 @@ document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// swiper.js
