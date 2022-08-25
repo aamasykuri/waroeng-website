ffa4806444ac7e6d11ae8e271eb672693d7cc936
marginkuri <aagung53@gmail.com>
2022-08-25 13:35:30 +0700

    add review section

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,37 @@
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    640: {
+      slidesPerView: 2,
     },
-    loop: true,
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
